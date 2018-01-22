694688bbb896722543f2c5932d0b1e0bf2b36399
pawoio <ppmlyniec@gmail.com>
2018-01-22 12:24:43 +0100

    coś

--- main.c
+++ main.c
@@ -9,21 +9,20 @@
 
 void render(void);
 void keyboard(unsigned char c,int x, int y);
-void mouse(int button, int state, int x, int y);
-void barnsleyFern( unsigned long iter);
-void count(int diceThrow, float* x0, float* y0, float* x1, float* y1);
-void fern(int , int , int , int );
-int p1=85,p2=7,p3=7,p4=1;
+
+
+void fern(long int , long int ,long int ,long int );
+long int p1=85,p2=7,p3=7,p4=1;
+long int p12,p123;
 int width, hight;
-int iterations=500;
- void putPixel(float x, float y);
+long int iterations=500;
+void putPixel(double x, double y);
 int main(int argc, char**argv)
 {
     //unsigned long num;
 
    // printf("Enter number of iterations : ");
-   // scanf("%ld",&num);
-
+   // scanf("%ld",&num)
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
     glutInitWindowPosition(100, 100);
@@ -35,17 +34,8 @@ int main(int argc, char**argv)
     glutDisplayFunc(render);
 
     glutKeyboardFunc(keyboard);
-    glutMouseFunc(mouse);
+
     glutMainLoop();
-
-
-
-
-
-
-   // barnsleyFern(500,num);
-
-
 
     return 0;
 }
@@ -57,8 +47,7 @@ void keyboard(unsigned char c, int x, int y)
     if(c == 'a')
     {
         printf("introduce 4 numbers which sum to 100 after spacebar :");
-        scanf("%d %d %d %d",&p1,&p2,&p3,&p4);
-        //iterations+=1000;
+        //scanf("%d %d %d %d",&p1,&p2,&p3,&p4);
         glutPostRedisplay();
     }
     if(c == 'd')
@@ -86,107 +75,31 @@ void keyboard(unsigned char c, int x, int y)
 
 }
 
-void mouse(int button, int state, int x, int y)
-{
-    if(button == GLUT_RIGHT_BUTTON)
-    {
-        /*if(iterations >= 100000) iterations-=100000;
-        else if (iterations == 0) iterations=0;
-        else if (iterations <= 1000) iterations-=100;
-        else if(iterations <= 20000) iterations-=1000;
-        else iterations-=10000;
-
-        if (iterations <= 0) iterations=0;*/
-
-    }
-    if(button == GLUT_LEFT_BUTTON)
-    {
-        /*if(iterations > 100000) iterations+=100000;
-        else if(iterations >= 20000) iterations+=10000;
-        else if (iterations >= 1000) iterations+=1000;
-        else iterations+=100;*/
-        //glutDisplayFunc(render);
-    }
-}
 
 void render(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+   // gluOrtho2D(0.0, 500.0, 0.0, 500.0);
     glBegin(GL_POINTS);
     glColor3f(0,1,0);
-    fern(iterations,p1,(p1+p2),(p1+p2+p3));
-
+    p12=p1+p2;
+    p123=p12+p3;
+   // fern(0,p1,p12,p123);
+    glVertex2d(0.1,0.2);
+    for(int i = 0; i < 50; i++) {
+        glVertex2d(i/100, i/50);
+    }
 
     glEnd();
-
+    glFlush();
     glutSwapBuffers();
 }
 
-void barnsleyFern( unsigned long iter){
 
-    float x0=0,y0=0,x1,y1;
-    int diceThrow;
-    time_t t;
-    srand((unsigned)time(&t));
-
-    while(iter>0){
-        diceThrow = rand()%100;
-        //count(diceThrow,&x0,&y0,&x1,&y1);
-        if(diceThrow<p4){
-            x1 = 0;
-            y1 = 0.16*y0;
-        }
-
-        else if(diceThrow>=p4 && diceThrow<=p3){
-            x1 = -0.15*x0 + 0.28*y0;
-            y1 = 0.26*x0 + 0.24*y0 + 0.44;
-        }
-
-        else if(diceThrow>=(p4+p3) && diceThrow<=(p4+p3+p2)){
-            x1 = 0.2*x0 - 0.26*y0;
-            y1 = 0.23*x0 + 0.22*y0 + 1.6;
-        }
-
-        else{
-            x1 = 0.85*x0 + 0.04*y0;
-            y1 = -0.04*x0 + 0.85*y0 + 1.6;
-        }
-
-        glVertex2f(x1/2.6558 ,y1/5.01-1);
-
-        x0 = x1;
-        y0 = y1;
-
-        iter--;
-    }
-
-}
-/*void count(int diceThrow, float* x0, float* y0, float* x1, float* y1)
+ void putPixel(double x, double y)
 {
-    if(diceThrow<p4){
-        *x1 = 0;
-
-        *y1 = 0.16 * (*y0);
-    }
-
-    else if(diceThrow>=p4 && diceThrow<=p3){
-        *x1 = -0.15*(*x0) + 0.28*(*y0);
-        *y1 = 0.26*(*x0) + 0.24*(*y0) + 0.44;
-    }
-
-    else if(diceThrow>=(p4+p3) && diceThrow<=(p4+p3+p2)){
-        *x1 = 0.2*(*x0) - 0.26*(*y0);
-        *y1 = 0.23*(*x0) + 0.22*(*y0) + 1.6;
-    }
-
-    else{
-        *x1 = 0.85*(*x0) + 0.04*(*y0);
-        *y1 = -0.04*(*x0) + 0.85*(*y0) + 1.6;
-    }
-
-}*/
- void putPixel(float x, float y)
-{
-    glVertex2f(x,y);
+    printf("%lf %lf",x,y);
+    glVertex2d(x, y);
 }
